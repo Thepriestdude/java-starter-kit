f8cc9137db46e3fb16bf4264fc68509c633286b4
Oscar Söderlund <poscar@spotify.com>
2014-11-29 10:36:33 +0100

    Update to new API

---
+++ src/main/java/com/monkeymusicchallenge/starterkit/AI.java
@@ -0,0 +1,25 @@
+package com.monkeymusicchallenge.starterkit;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ThreadLocalRandom;
+
+import org.json.JSONObject;
+
+public class AI {
+
+  public Map<String, Object> move(final JSONObject gameState) {
+    // Go to http://github.com/monkey-music-challenge/core
+    // for more info about the rules of Monkey Music Challenge!
+
+    // TODO: You may want to do something smarter here
+    final Map<String, Object> nextCommand = new HashMap<String, Object>();
+    nextCommand.put("command", "move");
+    nextCommand.put("direction", randomDirection());
+    return nextCommand;
+  }
+
+  private String randomDirection() {
+    return new String[] {"up", "down", "left", "right"}[ThreadLocalRandom.current().nextInt(4)];
+  }
+}
