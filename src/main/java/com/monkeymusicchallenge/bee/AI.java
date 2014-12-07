05f8391a1e466c806fef00c097992eaca794cff3
Gustav Svalander <gsvalander@gmail.com>
2014-12-07 16:23:47 +0100

    Project killed

--- src/main/java/com/monkeymusicchallenge/bee/AI.java
+++ src/main/java/com/monkeymusicchallenge/bee/AI.java
@@ -11,21 +11,7 @@ public class AI {
 
 	TreeMap<Integer, ListEntity> targets = new TreeMap<Integer, ListEntity>();
 
-	//ArrayList<ListEntity> targets;
-
 	public AI(){
-		// Points are sorted by distance from our monkey.
-		/*targets = new PriorityQueue<ListEntity>(100, new Comparator<ListEntity>() {
-			public int compare(ListEntity a, ListEntity b) {
-				System.out.println("% Compared "+a+" size:" + pathToA.size() + ", "+b+" size:"+pathToB.size());
-				ArrayList<ListEntity> pathToA = MonkeyAStar.getShortestPath(ListBuilder.getCurrentPosition(), a);
-				ArrayList<ListEntity> pathToB = MonkeyAStar.getShortestPath(ListBuilder.getCurrentPosition(), b);
-				if (pathToA == null || pathToB == null) {
-					return 0;
-				}
-				return (pathToA.size() > pathToB.size() ? 1 : -1);
-			}
-		});*/
 
 	}
 
@@ -49,59 +35,79 @@ public class AI {
 
     */
 
-		// Get all goals and insert into queue
-		ArrayList<ListEntity> targetMusics = currentListBuilder.getEntitiesByType("song");
-		targetMusics.addAll(currentListBuilder.getEntitiesByType("album"));
-		targetMusics.addAll(currentListBuilder.getEntitiesByType("playlist"));
-
-		System.out.println("! Possible targets list: "+ targetMusics);
-
-		targets.clear();
-		ListEntity nextNode;
-		for (ListEntity music : targetMusics) {
-			if (music != null) {
-				ArrayList<ListEntity> path = MonkeyAStar.getShortestPath(music, ListBuilder.getCurrentPosition());
-				//System.out.println("# From "+ListBuilder.getCurrentPosition()+" to "+music+" path("+path+")");
-				if ( path != null ) {
-					targets.put(path.size(), music);
-					//System.out.println("# There is a path("+path+") to "+ music+" added to queue.");
-				}
-			}else{
-				System.out.println("!!! Panic - Music was null");
-			}
-		}
-		System.out.println("! Targets: "+targets+"");
-
 		final Map<String, Object> nextCommand = new HashMap<String, Object>();
+		ArrayList<ListEntity> pathToTarget = null;
+		ListEntity nextNodeInPath = null;
+		ListEntity currentTarget = null;
 
-		if (targets.size()>0) {
-			// Execute against first in queue
-			targets.remove(targets.firstKey());
-			Integer lk = targets.firstKey();
-			ListEntity currentTarget = targets.get(lk);
+		if (inventory.size()<3) {
+			// Get all goals and insert into queue
+			ArrayList<ListEntity> targetMusics = currentListBuilder.getEntitiesByType("song");
+			targetMusics.addAll(currentListBuilder.getEntitiesByType("album"));
+			targetMusics.addAll(currentListBuilder.getEntitiesByType("playlist"));
 
-			if (currentTarget.equals(ListBuilder.getCurrentPosition())) {
-				System.out.println("< We have reached the target, picking a new one");
-				targets.remove(lk);
+			System.out.println("! Possible targets list: "+ targetMusics);
+
+			targets.clear();
+			ListEntity nextNode;
+			for (ListEntity music : targetMusics) {
+				if (music != null) {
+					ArrayList<ListEntity> path = ListBuilder.DFS(music);
+					//System.out.println("# From "+ListBuilder.getCurrentPosition()+" to "+music+" path("+path+")");
+					if ( path != null ) {
+						targets.put(path.size(), music);
+						//System.out.println("# There is a path("+path+") to "+ music+" added to queue.");
+					}
+				}else{
+					System.out.println("!!! Panic - Music was null");
+				}
 			}
-
-			ArrayList<ListEntity> pathToTarget = MonkeyAStar.getShortestPath(currentTarget, ListBuilder.getCurrentPosition());
-			System.out.println("- Current path to target: ["+pathToTarget+"]");
-
-			if (pathToTarget != null && pathToTarget.size() > 0 && (nextNode = pathToTarget.get(0)) != null ) {
-				// Decide how to move based on direction of the target
-				String move = Navigation.getDirection(nextNode);
-				nextCommand.put("command", "move");
-				nextCommand.put("direction",  move);
-				System.out.println("- Moving " + move + " to " + nextNode);
-			}else{
-				nextCommand.put("command", "idle");
-				System.out.println("- No path to target, idle");
-			}
+			System.out.println("! Targets: "+targets+"");
 		}else{
-			nextCommand.put("command", "idle");
-			System.out.println("- No path to target, idle");
+
 		}
+
+		currentTarget = targets.firstEntry().getValue();
+
+		// Inventory is full, goto user
+		pathToTarget = ListBuilder.DFS(nearestUser);
+		if (pathToTarget != null) {
+			System.out.println("- Moving to user "+currentTarget+" on path:"+pathToTarget);
+			nextNodeInPath = pathToTarget.values().toArray()[1];
+
+			String move = Navigation.getDirection(nextNodeInPath);
+			nextCommand.put("command", "move");
+			nextCommand.put("direction",  move);
+			return nextCommand;
+		}
+
+		// Just grab it
+		ListEntity pickup = Navigation.somethingNearBy(ListBuilder.getCurrentPosition());
+		if (pickup!=null) {
+			String move = Navigation.getDirection(pickup);
+			nextCommand.put("command", "move");
+			nextCommand.put("direction",  move);
+			System.out.println("- Picking up " + pickup + " to the " + move);
+			return nextCommand;
+		}
+
+		// We need to move along the path to music
+		pathToTarget = ListBuilder.DFS(currentTarget);
+		if(pathToTarget != null) {
+			System.out.println("- Moving to user "+currentTarget+" on path:"+pathToTarget);
+
+			nextNodeInPath = pathToTarget.values().toArray()[1];
+
+			String move = Navigation.getDirection(nextNodeInPath);
+			nextCommand.put("command", "move");
+			nextCommand.put("direction",  move);
+			return nextCommand;
+		}
+
+		// There is nothing we can do, just idle
+
+		nextCommand.put("command", "idle");
+		System.out.println("! No path to target, idle");
 
 		return nextCommand;
 	}
